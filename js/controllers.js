037eb411219106edb72d4ada5f7249131c8090b4
Huy Nguyen Quang <huyroberto@gmail.com>
2016-08-13 14:21:14 +0700

    Commit

--- js/controllers.js
+++ js/controllers.js
@@ -79,7 +79,7 @@ app.controller('AppCtrl', function ($scope, $ionicModal, $ionicPopover, $timeout
             });
             $window.location.reload(true);
             $state.go('app.home');
-            
+
         }
         function gotError(err) { // see more on error handling
             var alertPopup = $ionicPopup.alert({
@@ -120,28 +120,28 @@ app.controller('AppCtrl', function ($scope, $ionicModal, $ionicPopover, $timeout
             $state.go('app.login');
         }
     })
-    .controller('SettingsCtrl', function($scope, $ionicPopup, $ionicActionSheet){
-         //show actionsheet
-            $scope.showActionsheet = function () {
-                $ionicActionSheet.show({
-                    	// titleText: 'ActionSheet Example',
-                        buttons: [
-                            { text: '<i class="icon ion-social-facebook"></i>Facebook' },
-                            { text: '<i class="icon ion-social-twitter"></i>Twitter' },
-                            { text: '<i class="icon ion-more"></i>More' }, 
-                        ],
-                        buttonClicked: function(index) {
-                            // alert('BUTTON CLICKED' + index);
-                            return true;
-                        },
-                        cancelText: 'Cancel',
-                        cancel: function() {
-                            console.log('CANCELLED');
-                            }
-                });
-            }
-         // An alert dialog
-            $scope.showAlert = function() {
+    .controller('SettingsCtrl', function ($scope, $ionicPopup, $ionicActionSheet) {
+        //show actionsheet
+        $scope.showActionsheet = function () {
+            $ionicActionSheet.show({
+                // titleText: 'ActionSheet Example',
+                buttons: [
+                    { text: '<i class="icon ion-social-facebook"></i>Facebook' },
+                    { text: '<i class="icon ion-social-twitter"></i>Twitter' },
+                    { text: '<i class="icon ion-more"></i>More' },
+                ],
+                buttonClicked: function (index) {
+                    // alert('BUTTON CLICKED' + index);
+                    return true;
+                },
+                cancelText: 'Cancel',
+                cancel: function () {
+                    console.log('CANCELLED');
+                }
+            });
+        }
+        // An alert dialog
+        $scope.showAlert = function () {
             var alertPopup = $ionicPopup.alert({
                 title: 'My Salon v. 1.0.0<br/>',
                 template: '<center>Copyright 2016<br/>My Company, Inc.<br/>All rights reserved.</center>'
@@ -152,18 +152,18 @@ app.controller('AppCtrl', function ($scope, $ionicModal, $ionicPopover, $timeout
             });
         };
     })
-    .controller('QRCtrl',function ($scope, $timeout, $ionicScrollDelegate) {
+    .controller('QRCtrl', function ($scope, $timeout, $ionicScrollDelegate) {
         $scope.QR = true;
-        $scope.hide = function(){
+        $scope.hide = function () {
             $scope.QR = true;
         };
-        $scope.show = function (){
+        $scope.show = function () {
             $scope.QR = false;
         };
     })
-    .controller('ShakeCtrl',function ($scope, $timeout, $ionicScrollDelegate) {
+    .controller('ShakeCtrl', function ($scope, $timeout, $ionicScrollDelegate) {
         $scope.shake = true;
-        $scope.hide = function(){
+        $scope.hide = function () {
             $scope.shake = false;
         };
     })
@@ -174,15 +174,15 @@ app.controller('AppCtrl', function ($scope, $ionicModal, $ionicPopover, $timeout
             $state.go('app.login');
         }
     })
-    .controller('ScheduleCtrl', function ($scope, $state, $window, $rootScope, $stateParams) {
+    .controller('ScheduleCtrl', function ($scope, $state, $window, $rootScope, $stateParams, $compile, uiCalendarConfig) {
         if (!$rootScope.islogin) {
             $state.go('app.login');
         }
         $scope.calendar = true;
-        $scope.hide = function(){
+        $scope.hide = function () {
             $scope.calendar = true;
         };
-        $scope.show = function (){
+        $scope.show = function () {
             $scope.calendar = false;
         };
         $("#newcalendar").ionCalendar({
@@ -194,4 +194,132 @@ app.controller('AppCtrl', function ($scope, $ionicModal, $ionicPopover, $timeout
                 console.log(date);
             }
         });
+
+        //calendar
+        var date = new Date();
+        var d = date.getDate();
+        var m = date.getMonth();
+        var y = date.getFullYear();
+
+        $scope.changeTo = 'Hungarian';
+        /* event source that pulls from google.com */
+        $scope.eventSource = {
+            url: "http://www.google.com/calendar/feeds/usa__en%40holiday.calendar.google.com/public/basic",
+            className: 'gcal-event',           // an option!
+            currentTimezone: 'America/Chicago' // an option!
+        };
+        /* event source that contains custom events on the scope */
+        $scope.events = [
+            { title: 'All Day Event', start: new Date(y, m, 1) },
+            { title: 'Long Event', start: new Date(y, m, d - 5), end: new Date(y, m, d - 2) },
+            { id: 999, title: 'Repeating Event', start: new Date(y, m, d - 3, 16, 0), allDay: false },
+            { id: 999, title: 'Repeating Event', start: new Date(y, m, d + 4, 16, 0), allDay: false },
+            { title: 'Birthday Party', start: new Date(y, m, d + 1, 19, 0), end: new Date(y, m, d + 1, 22, 30), allDay: false },
+            { title: 'Click for Google', start: new Date(y, m, 28), end: new Date(y, m, 29), url: 'http://google.com/' }
+        ];
+        /* event source that calls a function on every view switch */
+        $scope.eventsF = function (start, end, timezone, callback) {
+            var s = new Date(start).getTime() / 1000;
+            var e = new Date(end).getTime() / 1000;
+            var m = new Date(start).getMonth();
+            var events = [{ title: 'Feed Me ' + m, start: s + (50000), end: s + (100000), allDay: false, className: ['customFeed'] }];
+            callback(events);
+        };
+
+        $scope.calEventsExt = {
+            color: '#f00',
+            textColor: 'yellow',
+            events: [
+                { type: 'party', title: 'Lunch', start: new Date(y, m, d, 12, 0), end: new Date(y, m, d, 14, 0), allDay: false },
+                { type: 'party', title: 'Lunch 2', start: new Date(y, m, d, 12, 0), end: new Date(y, m, d, 14, 0), allDay: false },
+                { type: 'party', title: 'Click for Google', start: new Date(y, m, 28), end: new Date(y, m, 29), url: 'http://google.com/' }
+            ]
+        };
+        /* alert on eventClick */
+        $scope.alertOnEventClick = function (date, jsEvent, view) {
+            $scope.alertMessage = (date.title + ' was clicked ');
+        };
+        /* alert on Drop */
+        $scope.alertOnDrop = function (event, delta, revertFunc, jsEvent, ui, view) {
+            $scope.alertMessage = ('Event Droped to make dayDelta ' + delta);
+        };
+        /* alert on Resize */
+        $scope.alertOnResize = function (event, delta, revertFunc, jsEvent, ui, view) {
+            $scope.alertMessage = ('Event Resized to make dayDelta ' + delta);
+        };
+        /* add and removes an event source of choice */
+        $scope.addRemoveEventSource = function (sources, source) {
+            var canAdd = 0;
+            angular.forEach(sources, function (value, key) {
+                if (sources[key] === source) {
+                    sources.splice(key, 1);
+                    canAdd = 1;
+                }
+            });
+            if (canAdd === 0) {
+                sources.push(source);
+            }
+        };
+        /* add custom event*/
+        $scope.addEvent = function () {
+            $scope.events.push({
+                title: 'Open Sesame',
+                start: new Date(y, m, 28),
+                end: new Date(y, m, 29),
+                className: ['openSesame']
+            });
+        };
+        /* remove event */
+        $scope.remove = function (index) {
+            $scope.events.splice(index, 1);
+        };
+        /* Change View */
+        $scope.changeView = function (view, calendar) {
+            uiCalendarConfig.calendars[calendar].fullCalendar('changeView', view);
+        };
+        /* Change View */
+        $scope.renderCalender = function (calendar) {
+            if (uiCalendarConfig.calendars[calendar]) {
+                uiCalendarConfig.calendars[calendar].fullCalendar('render');
+            }
+        };
+        /* Render Tooltip */
+        $scope.eventRender = function (event, element, view) {
+            element.attr({
+                'tooltip': event.title,
+                'tooltip-append-to-body': true
+            });
+            $compile(element)($scope);
+        };
+        /* config object */
+        $scope.uiConfig = {
+            calendar: {
+                height: 450,
+                editable: true,
+                header: {
+                    left: 'title',
+                    center: '',
+                    right: 'today prev,next'
+                },
+                eventClick: $scope.alertOnEventClick,
+                eventDrop: $scope.alertOnDrop,
+                eventResize: $scope.alertOnResize,
+                eventRender: $scope.eventRender
+            }
+        };
+
+        $scope.changeLang = function () {
+            if ($scope.changeTo === 'Hungarian') {
+                $scope.uiConfig.calendar.dayNames = ["Vasárnap", "Hétfő", "Kedd", "Szerda", "Csütörtök", "Péntek", "Szombat"];
+                $scope.uiConfig.calendar.dayNamesShort = ["Vas", "Hét", "Kedd", "Sze", "Csüt", "Pén", "Szo"];
+                $scope.changeTo = 'English';
+            } else {
+                $scope.uiConfig.calendar.dayNames = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+                $scope.uiConfig.calendar.dayNamesShort = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+                $scope.changeTo = 'Hungarian';
+            }
+        };
+        /* event sources array*/
+        $scope.eventSources = [$scope.events, $scope.eventSource, $scope.eventsF];
+        $scope.eventSources2 = [$scope.calEventsExt, $scope.eventsF, $scope.events];
     });
